dbf35a75cea2811f85c6ba60c5b86e33d2c1dfd6
Evan-cell <60639510+Evan-cell@users.noreply.github.com>
2021-11-06 11:01:24 +0300

    database

--- config.py
+++ config.py
@@ -4,6 +4,7 @@ class Config:
 
     
     SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:malcomiz0582@localhost/quotes'
 
 
 class ProdConfig(Config):
