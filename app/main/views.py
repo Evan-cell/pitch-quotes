88e47fdfe522ef831241a558e6eef36334b4c474
Evan-cell <60639510+Evan-cell@users.noreply.github.com>
2021-11-08 14:33:56 +0300

    base

--- app/main/views.py
+++ app/main/views.py
@@ -1,24 +1,12 @@
-from flask import render_template,request,redirect,url_for
+from flask import Flask, render_template,redirect,url_for
 from . import main
-# from flask_login import login_required
 
-# from .forms import ReviewForm
-# from ..models import Review
-
-
-# Views
 @main.route('/')
 def index():
+    return render_template('index.html')
 
-    '''
-    View root page function that returns the index page and its data
-    '''
 
-    message = 'Hello World'
-    return render_template('index.html',message = message)
-@main.route('/PITCHER/comment/new/<int:id>', methods = ['GET','POST'])
-def new_review(id):
-    
 
-    
-    return render_template('comment.html')
+@main.route('/pitches')
+def pitches():
+    return render_template('pitches.html')
